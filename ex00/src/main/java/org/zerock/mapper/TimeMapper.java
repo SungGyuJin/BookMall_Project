c0d9b8a61e7bfdc9e44679cc91ae4abb6d9e8d14
sgj <vkdnjwkrk3@naver.com>
2022-02-23 19:20:32 +0900

    0223 스프링 커밋

--- ex00/src/main/java/org/zerock/mapper/TimeMapper.java
+++ ex00/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -4,6 +4,9 @@ import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
 	
-	@Select("Select sysdate From dual")
+	@Select("SELECT sysdate FROM dual")
 	public String getTime();
+	
+	
+	public String getTime2();
 }
