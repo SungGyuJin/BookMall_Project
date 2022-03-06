ccc1de4907eae0fd5190047b83a640bd13902928
sgj <vkdnjwkrk3@naver.com>
2022-03-06 18:42:06 +0900

    0306 (1)커밋

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -18,7 +18,7 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @AllArgsConstructor
 public class BoardController {
-
+	
 	private BoardService service;
 
 	@GetMapping("/list")
@@ -28,47 +28,46 @@ public class BoardController {
 		model.addAttribute("list", service.getList());
 	}
 	
-	
-	//	@PostMapping("/register")
-//	public String register(BoardVO board, RedirectAttributes rttr) {
-//
-//		log.info("register: " + board);
-//
-//		service.register(board);
-//
-//		rttr.addFlashAttribute("result", board.getBno());
-//
-//		return "redirect:/board/list";
-//	}
-//
-//	@GetMapping("/get")
-//	public void get(@RequestParam("bno") Long bno, Model model) {
-//
-//		log.info("/get");
-//		model.addAttribute("board", service.get(bno));
-//	}
-//
-//	@PostMapping("/modify")
-//	public String modify(BoardVO board, RedirectAttributes rttr) {
-//
-//		log.info("modify: " + board);
-//
-//		if (service.modify(board)) {
-//			rttr.addFlashAttribute("result", "success");
-//		}
-//
-//		return "redirect:/board/list";
-//	}
-//
-//	@PostMapping("/remove")
-//	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
-//
-//		log.info("remove..." + bno);
-//		if (service.remove(bno)) {
-//			rttr.addFlashAttribute("result", "success");
-//		}
-//
-//		return "redirect:/board/list";
-//	}
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+
+		log.info("register: " + board);
+
+		service.register(board);
+
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+
+		log.info("modify: " + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list";
+	}
+
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+
+		log.info("remove..." + bno);
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list";
+	}
 
 }
