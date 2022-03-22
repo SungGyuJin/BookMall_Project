376f180bf022e6a1e0c20e15d774745d2294d75e
sgj <vkdnjwkrk3@naver.com>
2022-03-22 19:41:03 +0900

    0322 (1)커밋

--- ex02/src/main/java/org/zerock/service/BoardServiceTests.java
+++ ex02/src/main/java/org/zerock/service/BoardServiceTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -30,18 +31,20 @@ public class BoardServiceTests {
 	public void testRegister() {
 
 		BoardVO board = new BoardVO();
-		board.setTitle("»õ·Î ÀÛ¼ºÇÏ´Â ±Û");
-		board.setContent("»õ·Î ÀÛ¼ºÇÏ´Â ³»¿ë");
+		board.setTitle("ï¿½ï¿½ï¿½ï¿½ ï¿½Û¼ï¿½ï¿½Ï´ï¿½ ï¿½ï¿½");
+		board.setContent("ï¿½ï¿½ï¿½ï¿½ ï¿½Û¼ï¿½ï¿½Ï´ï¿½ ï¿½ï¿½ï¿½ï¿½");
 		board.setWriter("newbie");
 
 		service.register(board);
 
-		log.info("»ý¼ºµÈ °Ô½Ã¹°ÀÇ ¹øÈ£" + board.getBno());
+		log.info("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ô½Ã¹ï¿½ï¿½ï¿½ ï¿½ï¿½È£" + board.getBno());
 	}
 
 	@Test
 	public void testGetList() {
-		service.getList().forEach(board -> log.info(board));
+	
+//		service.getList().forEach(board -> log.info(board));
+		service.getList(new Criteria(2, 10)).forEach(board -> log.info(board));
 	}
 
 	@Test
@@ -66,7 +69,7 @@ public class BoardServiceTests {
 			return;
 		}
 
-		board.setTitle("Á¦¸ñ ¼öÁ¤ÇÕ´Ï´Ù.");
+		board.setTitle("ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Õ´Ï´ï¿½.");
 		log.info("MODIFY RESULT: " + service.modify(board));
 	}
 
