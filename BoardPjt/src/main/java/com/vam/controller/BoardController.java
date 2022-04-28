0c52dd6e9496172c336be1da94675ff92af52dac
sgj <vkdnjwkrk3@naver.com>
2022-04-28 18:24:45 +0900

    0428 (1)커밋

--- BoardPjt/src/main/java/com/vam/controller/BoardController.java
+++ BoardPjt/src/main/java/com/vam/controller/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vam.model.BoardVO;
 import com.vam.service.BoardService;
@@ -33,11 +34,13 @@ public class BoardController {
 	}
 	
 	@PostMapping("/enroll")
-	public String boardEnrollPOST(BoardVO board) {
+	public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
 		
 		log.info("BoardVO : " + board);
 		
 		bservice.enroll(board);
+		
+		rttr.addFlashAttribute("result", "enrol success");
 		
 		return "redirect:/board/list";
 	}
--- BoardPjt/src/main/webapp/WEB-INF/views/board/list.jsp
+++ BoardPjt/src/main/webapp/WEB-INF/views/board/list.jsp
@@ -1,13 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
 	<h1>목록페이지</h1>
 	<a href="/board/enroll">등록하기</a>
 </body>
+<script>
+	$(document).ready(function(){
+		
+		let result = '<c:out value="${result}" />';
+		
+		checkAlert(result);
+		
+		function checkAlert(result){
+			
+			if(result === ''){
+				return;
+			}
+			
+			if(result === "enrol success"){
+				alert("등록완료!!");
+			}
+		}
+		
+		
+	});
+
+</script>
 </html>
